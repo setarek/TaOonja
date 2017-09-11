0a136631d78ee518aec96a1a6ec24ed3e7d4c613
Setare Karimi <setarekarimi.kh@gmail.com>
2017-09-11 22:53:15 +0430

    Change model File to Show Better and Correct Image Field

--- taOonja/game/models.py
+++ taOonja/game/models.py
@@ -2,16 +2,21 @@ import os
 
 from django.db import models
 
-def get_image_path(filename):
-    return os.path.join('photos',filename)
+#def get_image_path(filename):
+#    return os.path.join('media')
 
 class Location(models.Model):
     name = models.CharField(max_length=250)
     local_name = models.CharField(max_length=250)
     visited = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 class Detail(models.Model):
     coordinates = models.CharField(max_length=250)
     detail = models.CharField(max_length=500)
-    img = ImageField(upload_to=get_image_path, blank=True, null=True)
+    img = models.ImageField(upload_to = "media/", blank=True, null=True)
     location = models.OneToOneField(Location, on_delete=models.CASCADE, primary_key=True)
+    def __str__(self):
+        return self.detail
