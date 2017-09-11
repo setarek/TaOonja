a7a5ed0b7b88849dc2ea10c53939c63dd70acd0a
Setare Karimi <setarekarimi.kh@gmail.com>
2017-09-11 22:54:02 +0430

    Create Basic View

--- taOonja/game/views.py
+++ taOonja/game/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from game.models import *
+class LocationListView(ListView):
 
-# Create your views here.
+    template_name = 'game/location_list.html'
+    context_object_name = 'location_list'
+
+    def get_queryset(self):
+        return Detail.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(LocationListView, self).get_context_data(**kwargs)
+        return context
+
+
+
+#class DetailDetailView(DetailView):
+##
+#    context_object_name = 'location_detail'
+#    def get_queryset(self):
+#        return Detail.objects.get(location=)
