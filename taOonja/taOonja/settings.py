b828232c7f7787ff0e11660d016b738f6420b8b8
Setare Karimi <setarekarimi.kh@gmail.com>
2017-09-11 21:24:46 +0430

    Add static files ,Templates and Change Some setting Config

--- taOonja/taOonja/settings.py
+++ taOonja/taOonja/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'taOonja.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
