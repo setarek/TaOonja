1a93c58e278712a2c52f36b098a570a7f48c7ef2
Setare Karimi <setarekarimi.kh@gmail.com>
2017-09-15 12:54:35 +0430

    Change View According to model Changes

--- taOonja/game/views.py
+++ taOonja/game/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from game.models import *
+from game.models import Location
 class LocationListView(ListView):
 
     template_name = 'game/location_list.html'
     context_object_name = 'location_list'
 
     def get_queryset(self):
-        return Detail.objects.all()
+        return Location.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(LocationListView, self).get_context_data(**kwargs)
@@ -21,8 +21,9 @@ class LocationDetailView(DetailView):
     model = Location
 
     context_object_name = 'location_detail'
-
+    
     def get_context_data(self, **kwargs):
+        l_pk = self.kwargs['pk']
+        Location.objects.filter(pk=l_pk).update(visited = True)
         context = super(LocationDetailView, self).get_context_data(**kwargs)
-        context['detail_info'] = Detail.objects.all()
         return context
