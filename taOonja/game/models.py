a4f1c62301dd91e20cdd71d5572db7f564b61e9e
Javid Izadfar <Javid.Izadfar@gmail.com>
2017-10-26 23:58:26 +0330

    Added Detail Support with Markdown

--- taOonja/game/models.py
+++ taOonja/game/models.py
@@ -1,13 +1,14 @@
 import os
 
 from django.db import models
+from django import forms
 
 class Location(models.Model):
     name = models.CharField(max_length=250)
     local_name = models.CharField(max_length=250)
     visited = models.BooleanField(default=False)
     coordinates = models.CharField(max_length=250)
-    detail = models.CharField(max_length=500)
+    detail = models.TextField()
     img = models.ImageField(upload_to = "media/", blank=True, null=True)
 
     def __str__(self):
